989558ecd6ffaedf533166d63f8d54f66736a714
gregkaf <gregori.kafanov@mbicycle.com>
2022-06-02 16:45:41 +0300

    Refactor extensions]

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import router from "./endpoints/template/route";
+import router from "./endpoints/template/route.js";
 
 const app = express();
 
--- endpoints/template/route.js
+++ endpoints/template/route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getTemplate } from "./controller";
+import { getTemplate } from "./controller.js";
 
 const router = express.Router();
 
