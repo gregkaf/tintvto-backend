05d009195658bc859acffdf8c0d2ba6fced078ca
gregkaf <gregori.kafanov@mbicycle.com>
2022-06-02 16:51:44 +0300

    Updates minors

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ import router from "./endpoints/template/route.js";
 
 const app = express();
 
+// settings middlewares
 app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.urlencoded({extended: true})); //prettier-ignore
 
+// roures
 app.use("/template", router);
 
 export default app;
